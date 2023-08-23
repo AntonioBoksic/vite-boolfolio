f6b0d7b41c7e46992a2ee8e0fea10180ef3d98af
Antonio <antonioboksic1603@gmail.com>
2023-08-23 16:21:16 +0200

    first commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <h1>Projects</h1>
+
+  <div class="row justify-content-between">
+    <ProjectCard
+    v-for="project in projects"
+    :key="project.id"
+    :project="project"
+
+    class="col-3 my-2"
+    />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import ProjectCard from './components/ProjectCard.vue'
+import axios from 'axios';
+
+export default {
+  name: 'app',
+
+  components: {
+    ProjectCard
+  },
+
+  data() {
+    return {
+      projects: []
+    }
+  },
+
+  mounted() {
+    axios.get("http://127.0.0.1:8000/api/v1/projects")
+    .then(response => {
+
+      console.log(response.data.projects)
+
+      this.projects = response.data.projects
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+
+}
+</script>
+
+
+
+<style scoped>
+
+
+
+</style>
