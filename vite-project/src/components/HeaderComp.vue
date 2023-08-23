fc28cd58962bcb727876a1abcf3c17fbd734a739
Antonio <antonioboksic1603@gmail.com>
2023-08-23 20:56:57 +0200

    aggiunto un po di stile all header che almeno si capisce meglio che è un header

--- vite-project/src/components/HeaderComp.vue
+++ vite-project/src/components/HeaderComp.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>Projects in Portfolio</h1>
+    <header>
+        <h1>Projects in Portfolio</h1>
+    </header>
+    
 </template>
+
+<style>
+header {
+    background-color: black;
+    color: white;
+}
+</style>
