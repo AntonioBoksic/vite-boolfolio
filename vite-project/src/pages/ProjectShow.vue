f0ae686b119654467b8cff18b4b7aa6faad59e75
Antonio <antonioboksic1603@gmail.com>
2023-08-23 20:51:31 +0200

    creato componente ProjectShow per il dettaglio dei singoli progetti.

---
+++ vite-project/src/pages/ProjectShow.vue
@@ -0,0 +1,41 @@
+<template>
+
+<h1>Project SHOW</h1>
+
+<h2>Project {{ project.name }}</h2>
+<p>{{ project.description }}</p>
+<div class="row">
+    <span class="col">device: {{ project.device }}</span>
+    <span class="col">programming language: {{ project.programming_language }}</span>
+
+</div>
+
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'ProjectShow',
+    data() {
+        return {
+            project: {}
+        }
+    },
+    mounted() {
+        const projectId = this.$route.params.id;
+        // console.log(projectId)
+        axios.get("http://127.0.0.1:8000/api/v1/projects/" + projectId)
+        .then(res => {
+
+            const data = res.data.project;
+            console.log(data)
+            this.project = data
+        })
+        .catch(err => {
+            console.log(err)
+
+        })
+    }
+}
+</script>
